c08f08e3536c44dc4df57e6799f13e258b52d234
Hayoung Kang <rkdgkdudsam@naver.com>
2025-06-25 14:30:12 +0900

    style: 파티 생성 버튼 애니메이션 수정

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <>
             <div className="h-[100px] w-full justify-center flex items-center">
-                <button className="h-[50px] w-[160px] rounded-full bg-black text-white pt-1 text-xl hover:bg-gray-500 duration-300">
+                <button className="h-[50px] w-[160px] rounded-full bg-black text-white text-xl hover:scale-105 active:scale-95 duration-200">
                     파티 생성
                 </button>
             </div>
