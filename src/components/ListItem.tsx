e2ba7844c42cbe2030ffaa820f8643e1dca02482
Hayoung Kang <rkdgkdudsam@naver.com>
2025-06-25 14:28:58 +0900

    feat: 사람 삭제 로직 구현

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,7 +1,14 @@
 import { FaRegTrashAlt } from "react-icons/fa";
 import ListCharacter from "./ListCharacter";
+import { useListStore } from "../store/listStore";
 
 export default function ListItem({ user }: { user: User }) {
+    const { deleteUser } = useListStore();
+
+    const handleDeleteUser = () => {
+        deleteUser(user.name);
+    };
+
     return (
         <>
             <li className="min-h-[100px] w-full rounded-2xl shadow-[0_2px_6px_rgba(0,0,0,0.5)] grid grid-cols-[10%_80%_10%] items-center cursor-pointer">
@@ -16,7 +23,10 @@ export default function ListItem({ user }: { user: User }) {
                         이곳을 눌러 캐릭터를 등록해주세요.
                     </div>
                 )}
-                <div className="justify-self-center p-2 rounded-md hover:bg-gray-100 duration-200">
+                <div
+                    className="justify-self-center p-2 rounded-md hover:bg-gray-100 duration-200"
+                    onClick={handleDeleteUser}
+                >
                     <FaRegTrashAlt size={20} color="red" />
                 </div>
             </li>
