26b5391d2e747c4667e45e431328c2a401ab60c5
Hayoung Kang <rkdgkdudsam@naver.com>
2025-06-27 03:24:52 +0900

    feat: 파티 생성 API 연결

---
+++ src/apis/party.ts
@@ -0,0 +1,26 @@
+import { isBuffer } from "../utils/isBuffer";
+
+/* 파티 생성 */
+export const getParty = async (list: User[]) => {
+    const response = await fetch("http://3.36.58.16:8080/api/v2/party/create", {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(
+            list.flatMap((user: User) =>
+                user.characters.map((character: Character) => ({
+                    characterName: character.characterName,
+                    characterId: character.characterId,
+                    jobName: character.jobName,
+                    ownedName: user.name,
+                    isBuffer: isBuffer(character.jobName),
+                    serverId: character.serverId,
+                }))
+            )
+        ),
+    });
+
+    const data = await response.json();
+    return data;
+};
